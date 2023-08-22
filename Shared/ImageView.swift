6c688447f7de1073c5d4a3f46e23857abe82aa8f
waramity <waramity.re@gmail.com>
2023-08-23 01:57:30 +0700

    add image example

--- Shared/ImageView.swift
+++ Shared/ImageView.swift
@@ -9,6 +9,33 @@ import SwiftUI
 
 struct ImageView: View {
     var body: some View {
-        Text("nahee")
+        ScrollView {
+            VStack {
+                Text("Image")
+                // .resizable() modifier to make it fit the entire available screen area
+                Image("test").resizable()
+                // image to fit within the screen edges and maintain its aspect ration
+                Image("test")
+                    .resizable()
+                    .scaledToFit()
+                // Position the image within a custom frame:
+                Image("test")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 150.0, height: 100.0, alignment: .top)
+                // Circle image and .aspectRatio(contentMode: .fill) == .scaledToFit()
+                Image("test")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 200.0, height: 200.0, alignment: .center)
+                    .clipShape(Circle())
+                Image("test")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100.0, height: 100.0, alignment: .center)
+                    .border(Color.blue, width: 3.0)
+                    .clipped()
+                
+            }}
     }
 }
