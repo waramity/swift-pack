463f332f7bb922c00fcf0f4f7f972a19518bde76
waramity <waramity.re@gmail.com>
2023-08-23 14:47:41 +0700

    add line multiline, limit, and spacing/padding

--- Shared/TextView.swift
+++ Shared/TextView.swift
@@ -13,9 +13,6 @@ struct TextView: View {
             VStack {
                 Group {
                     Text("Text")
-                    Text("font title")
-                        .font(.title)
-                        .fontWeight(.bold)
                     Text("bold").fontWeight(.bold)
                     Text("light").fontWeight(.light)
                     Text("black").fontWeight(.black)
@@ -24,10 +21,32 @@ struct TextView: View {
                     Text("regular").fontWeight(.regular)
                     Text("semibold").fontWeight(.semibold)
                     Text("thin").fontWeight(.thin)
+                    Text("title")
+                        .font(.title)
+                        .fontWeight(.bold)
                 }
                 Group {
+                    Text("rounded")
+                        .font(.system(.title, design: .rounded))
+                        .fontWeight(.bold)
+                    Text("size")
+                        .font(.system(size: 23))
+                        .fontWeight(.bold)
+                    Text("color")
+                        .foregroundColor(.green)
+                    Text("[Multipleline], so don’t waste it living someone else’s life. Don’t be trapped by dogma—which is living with the results of other people’s thinking. Don’t let the noise of others’ opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition.")
+                    Text("[Center multipleline], so don’t waste it living someone else’s life. Don’t be trapped by dogma—which is living with the results of other people’s thinking. Don’t let the noise of others’ opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition.")
+                        .multilineTextAlignment(.center)
+                    Text("[Line limit], so don’t waste it living someone else’s life. Don’t be trapped by dogma—which is living with the results of other people’s thinking. Don’t let the noise of others’ opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition.")
+                        .lineLimit(1)
                     
-                    Text("thin").fontWeight(.thin)
+                    Text("[Truncation mode], so don’t waste it living someone else’s life. Don’t be trapped by dogma—which is living with the results of other people’s thinking. Don’t let the noise of others’ opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition.")
+                        .lineLimit(1)
+                        .truncationMode(.middle)
+                    
+                    Text("[Line spacing and padding], so don’t waste it living someone else’s life. Don’t be trapped by dogma—which is living with the results of other people’s thinking. Don’t let the noise of others’ opinions drown out your own inner voice. And most important, have the courage to follow your heart and intuition.")
+                        .lineSpacing(10)
+                        .padding()
                 }
             }
         }
