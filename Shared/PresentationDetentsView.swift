d79c12884162cd89783d755d7015b356327acdf0
waramity <waramity.re@gmail.com>
2023-08-26 00:59:28 +0700

    add fraction and custom detent

--- Shared/PresentationDetentsView.swift
+++ Shared/PresentationDetentsView.swift
@@ -7,9 +7,19 @@
 
 import SwiftUI
 
+struct CustomDetent: CustomPresentationDetent {
+    // 1
+    static func height(in context: Context) -> CGFloat? {
+        // 2
+        return max(50, context.maxDetentValue * 0.1)
+    }
+}
+
 struct PresentationDetentsView: View {
     @State var basic = false
     @State var fix_height = false
+    @State var fraction = false
+    @State var custom_detent = false
     
     var body: some View {
         NavigationView {
@@ -20,6 +30,12 @@ struct PresentationDetentsView: View {
                 Button("Fix height") {
                     fix_height = true
                 }
+                Button("Fraction") {
+                    fraction = true
+                }
+                Button("Custom Detent") {
+                    custom_detent = true
+                }
             }
             .navigationTitle("Presentation Detents")
         }.sheet(isPresented: $basic) {
@@ -29,6 +45,12 @@ struct PresentationDetentsView: View {
         }.sheet(isPresented: $fix_height) {
             Text("Fix height view")
                 .presentationDetents([.height(250)])
+        }.sheet(isPresented: $fraction) {
+            Text("Fraction view")
+                .presentationDetents([.fraction(0.1)])
+        }.sheet(isPresented: $custom_detent) {
+            Text("Custom detent view")
+                .presentationDetents([.custom(CustomDetent.self)])
         }
     }
 }
