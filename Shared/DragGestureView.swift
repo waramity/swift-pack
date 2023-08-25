320d119ead62f90292793f51fa4c4fa60ba5e3d1
waramity <waramity.re@gmail.com>
2023-08-25 19:08:43 +0700

    add drag gesture view

--- Shared/DragGestureView.swift
+++ Shared/DragGestureView.swift
@@ -8,7 +8,25 @@
 import SwiftUI
 
 struct DragGestureView: View {
+    @GestureState private var dragOffset = CGSize.zero
+    @State private var position = CGSize.zero
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(systemName: "star.circle.fill")
+            .font(.system(size: 100))
+            .offset(x: position.width + dragOffset.width, y: position.height + dragOffset.height)
+            .animation(.easeInOut, value: dragOffset)
+            .foregroundColor(.green)
+            .gesture(
+                DragGesture()
+                    .updating($dragOffset, body: { (value, state, transaction) in
+                        
+                        state = value.translation
+                    })
+                    .onEnded({ (value) in
+                        self.position.height += value.translation.height
+                        self.position.width += value.translation.width
+                    })
+            )
     }
 }
